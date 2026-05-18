c37011e8ab4229a9ba4d60f3f7a409388aabf85e
Lucas4895 <23554ln@hvhs.school.nz>
2026-05-18 13:13:05 +1200

    18/May/2026 -- Testing google fb_authenticate() p2
    added fb_handleLogin()
    added fb_popupLogin()

--- fb_io.js
+++ fb_io.js
@@ -10,14 +10,27 @@ var GLOBAL_user;
 var authenticationListener; //global variable to store the listener
  
 function fb_authenticate(){
+    authenticationListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
+    // authenticate with Google
+}
+
+function fb_handleLogin(_user){
+    if (_user){
+        console.log("User is logged in")
+    } else {
+        console.log("User is Not logged in - Starting the popup process")
+        fb_popupLogin();
+    }
+}
+
+function fb_popupLogin(){
     var provider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().signInWithPopup(provider).then((result) => {
         GLOBAL_user = result.user; //save the user details object to a global variable
         console.log("User has logged in")
     });
-    // authenticate with Google
-}
+ };
 
 function fb_error(){
     // Don't forget your error handling!
