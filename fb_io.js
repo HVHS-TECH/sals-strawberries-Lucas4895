f7641c11cea3ae7696e8d7d1b24f95ba3a790c15
Lucas4895 <23554ln@hvhs.school.nz>
2026-05-19 15:01:17 +1200

    19/May/2026
    Testing fb_sendMail()

--- fb_io.js
+++ fb_io.js
@@ -8,7 +8,8 @@
  **************************************************************/
 var GLOBAL_user;
 var authenticationListener; //global variable to store the listener
- 
+const userEmail = GLOBAL_user.email
+
 function fb_authenticate(){
     authenticationListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
     // authenticate with Google
@@ -37,6 +38,10 @@ function fb_logout(){
     firebase.auth().signOut();
     console.log("logged out")
 };
+
+function fb_sendEmail(){
+    console.log("To: " + userEmail + ", welcome!")
+}
 
 function fb_error(){
     // Don't forget your error handling!
